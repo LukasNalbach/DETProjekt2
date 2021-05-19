1f7f473d281177751938e1e3af8469d831089d3c
JohannesLawnik <lawnik.johannes@gmail.com>
2021-05-19 17:34:52 +0200

    add task script

---
+++ Assets/Scripts/Task.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+    //Time to solve one Task(in Seconds)
+    public float timeToSolve;
+
+    public bool solvingVisible;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startSolving()
+    {
+
+    }
+
+    public void endSolving()
+    {
+
+    }
+}
